35ff7dd71658066134b7cc2b14a82e194daa9d00
Obel Sera <obels@hotmail.com>
2024-01-18 18:05:34 -0800

    improved homepage display and added link icon

--- src/components/home-page/home.jsx
+++ src/components/home-page/home.jsx
@@ -15,18 +15,21 @@ function Home(props) {
           height: 1500,
           width: 1500,
           textAlign: "center",
-          backgroundColor: "lightblue",
         }}
       >
         <h5>Welcome to Clark County Employee Website</h5>
-        <div
-          className="sidebar"
-          style={{ height: 700, width: 350, backgroundColor: "lightgray" }}
-        >
+        <div className="sidebar" style={{ height: 700, width: 350 }}>
           <div style={{ textAlign: "left" }}>
-            <h5> Employee Forms</h5>
-            <ul>
-              <Link to="/forms/overtime">Employee Overtime Form</Link>
+            <h6> Employee Forms</h6>
+            <ul className="list-unstyled">
+              <Link to="/forms/overtime">
+                <i
+                  class="fa fa-file-text"
+                  style={{ marginRight: 5 }}
+                  aria-hidden="true"
+                ></i>
+                Employee Overtime Form
+              </Link>
             </ul>
           </div>
         </div>
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -4,51 +4,53 @@ import navLogo from "./images/navbarLogo.png";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-md navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="home">
-          <img
-            src={navLogo}
-            style={{ height: 45, width: 45 }}
-            alt="navBar Logo"
-          />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink
-                className="nav-link active"
-                aria-current="page"
-                to="home"
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="directory">
-                Employee Directory
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="login">
-                Login
-              </NavLink>
-            </li>
-          </ul>
+    <div className="container" style={{ textAlign: "center" }}>
+      <nav className="navbar navbar-expand-md navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="home">
+            <img
+              src={navLogo}
+              style={{ height: 45, width: 45 }}
+              alt="navBar Logo"
+            />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link active"
+                  aria-current="page"
+                  to="home"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="directory">
+                  Employee Directory
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="login">
+                  Login
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 export default NavBar;
